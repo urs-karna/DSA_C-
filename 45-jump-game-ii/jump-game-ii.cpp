50bcf8176fd8211ab2e12838fc6953c1468d49e9
Harish Tholeti <147231266+urs-karna@users.noreply.github.com>
2025-07-25 18:18:05 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (36.50%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  
+    int jump(vector<int>& arr) {
+         int n=arr.size();
+         int i=0;
+         int min_step=0;
+         if(n==1 ) return 0;
+          int jump=0;
+
+          int l=0,r=0;
+          while(r< n-1)  {
+          int fartest=0;
+            
+          for(int i=l;i<=r;i++)  {
+             
+              fartest= max(i+arr[i],fartest);
+              
+          }
+           l = r+1;
+           r=fartest;
+           jump +=1;
+          }
+          return jump;
+    }
+};
