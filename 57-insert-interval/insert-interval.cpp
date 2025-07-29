0b7497dd864f2825034051abcfa0ae2d4fb90d09
Harish Tholeti <147231266+urs-karna@users.noreply.github.com>
2025-07-29 16:03:52 +0530

    Time: 7 ms (8.92%) | Memory: 22.9 MB (5.37%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-      vector<vector<int>>copyIntervals(intervals.begin(),intervals.end()); 
-     
-    
-
-        copyIntervals.push_back(newInterval);
-    
-    sort(copyIntervals.begin(),copyIntervals.end());
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newIntervals) {
+      vector<vector<int>>copyIntervals(intervals.begin(),intervals.end());  
    
-    vector<vector<int>>v;
+    vector<vector<int>>res;
+    int i=0;
+    int n=copyIntervals.size();
+  //left
+    while(i < n && copyIntervals[i][1] < newIntervals[0])   {
+          res.push_back(copyIntervals[i]);
+          i++;
+    }
+    //find overlapping 
 
-    for(int i=0;i<copyIntervals.size();i++)  {
+    while( i < n && copyIntervals[i][0] <= newIntervals[1]) {
 
-         if( v.size()==0 || copyIntervals[i][0] > v.back()[1] ){
+        newIntervals[0] = min (newIntervals[0], copyIntervals[i][0]);
+        newIntervals[1]  = max( newIntervals[1], copyIntervals[i][1]);
+        i++;
+    }
+    res.push_back(newIntervals);
 
+    while(i< n) {
+     res.push_back(copyIntervals[i]);
+     i++;
 
-            v.push_back(copyIntervals[i]);
-         }
-         else {
-
-            v.back()[1]= max(v.back()[1], copyIntervals[i][1]);
-         }
 
     }
-    return v;
+
+    return res;
 
     }
 };
