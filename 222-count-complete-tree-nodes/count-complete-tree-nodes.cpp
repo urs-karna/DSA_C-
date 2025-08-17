82b9096485a525fbd732f7e69c4ff876898d98bc
Harish Tholeti <147231266+urs-karna@users.noreply.github.com>
2025-08-17 07:06:20 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (94.17%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -11,26 +11,37 @@
  */
 class Solution {
 public:
-    void no_of_nodes(TreeNode* root,int &cnt) {
-           if(root==NULL) return;
-
-           if(root->left){
-            cnt++;
-             no_of_nodes(root->left,cnt);
-           }
-            if(root->right){
-            cnt++;
-             no_of_nodes(root->right,cnt);
-           }
-
-    }
     int countNodes(TreeNode* root) {
         if(root==NULL) return 0;
-      //  if(root) return 1;
-
-        int cnt=0;
-        no_of_nodes(root,cnt);
-        return cnt+1;
         
+        int lh=HeightofLeft(root);
+
+        int rh=HeightofRight(root);
+
+        if(lh==rh) {
+            cout<<( 1<< lh)-1<< endl;
+            return (1<<lh)-1;
+            
+        }
+        cout<<1+lh+rh<<endl;
+         return 1+ countNodes(root->left) + countNodes(root->right);
+      
+    }
+    int HeightofLeft(TreeNode* root) {
+        int cnt=0;
+        while(root) {
+            cnt++;
+             root=root->left;
+        }
+        return cnt;
+    }
+
+    int HeightofRight(TreeNode* root) {
+        int cnt=0;
+        while(root) {
+            cnt++;
+             root=root->right;
+        }
+        return cnt;
     }
 };
