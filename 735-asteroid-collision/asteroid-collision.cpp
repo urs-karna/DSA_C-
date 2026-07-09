75381098bbda76ed3e8b6299f00d7c985f4898c6
Harish Tholeti <147231266+urs-karna@users.noreply.github.com>
2026-07-09 18:43:47 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (89.82%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& arr) {
-      int n= arr.size();
-     // vector<int>ans;
-           vector<int> ans;
-        //int n = arr.size();
-        
-        for (int i = 0; i < n; ++i) {
-            if (arr[i] > 0) {
-                ans.push_back(arr[i]);
-            } else {
-                while (!ans.empty() && ans.back() > 0 && ans.back() < -arr[i]) {
-                    ans.pop_back();
-                }
-                if (!ans.empty() && ans.back() == -arr[i]) {
-                    ans.pop_back();
-                } else if (ans.empty() || ans.back() < 0) {
-                    ans.push_back(arr[i]);
-                }
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        vector<int>ans;
+         int n = asteroids.size();
+        for(int i =0 ; i< n;i++ ) {
+             
+            if( asteroids[i] > 0) {
+                ans.push_back(asteroids[i]);
+            } 
+            else {
+            while ( !ans.empty() && ans.back() > 0  && ans.back() < -(asteroids[i])){
+               ans.pop_back();
+               
             }
+
+             if (asteroids[i] < 0 && !ans.empty() && ans.back() == -(asteroids[i]))
+            {
+                ans.pop_back();
+            }
+            else if ( ans.empty() || ans.back() < 0   ) {
+
+                ans.push_back( asteroids[i]);
+
+            }
+            }
+  
         }
- 
-      return ans;  
+
+        return ans;
     }
 };
