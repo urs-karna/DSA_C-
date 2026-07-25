2ec01c3c3bdc97ffc426a8f1ff1aa6c63fa5764a
Harish Tholeti <147231266+urs-karna@users.noreply.github.com>
2026-07-25 09:08:22 +0530

    Time: 3 ms (74.98%) | Memory: 19.3 MB (68.06%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& edges) {
-
-        vector<int> inDegree(V, 0);
-        vector<vector<int>> adj(V);
-         queue<int> q;
-        for (int i = 0; i < edges.size(); i++) {
-
-            int u = edges[i][0];
-            int v = edges[i][1];
-
-            adj[v].push_back(u);
-            inDegree[u]++;
-        }
-
-        for(int i =0; i < V;i++ ) {
-            if(inDegree[i]==0) {
-                q.push(i);
-            }
-        }
-        int cnt = 0 ;
-       while(!q.empty())  {
-
-        int node = q.front();
-        cnt++;
-         q.pop();
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
           
-          for( auto it : adj[node] ) {
+        int V = numCourses;
 
-            inDegree[it]--;
-            if( inDegree[it] == 0) q.push(it);
-          }
-       }
+        vector<vector<int>>adj(V);
+        vector<int>inorder(V,0);
+      
+      for( int i =0; i<prerequisites.size();i++) {
+      
+        int u = prerequisites[i][0];
+        int v = prerequisites[i][1];
+        adj[v].push_back(u);
+        inorder[u]++;
+      
+      }
 
-       if (cnt == V)  return true;
-       return false ;
+      queue<int>q;
+      for(int i =0;i<V;i++) {
+
+        if(inorder[i] == 0){
+            q.push(i);
+        }
+      }
+
+      int cnt =0;
+
+      while(!q.empty()) {
+        
+        int node = q.front();
+        q.pop();
+        cnt++;
+      for ( auto it : adj[node]){
+
+        inorder[it]--;
+        if(inorder[it]==0 ) q.push(it);
+      }
+
+
+      }
+
+      return cnt== V;
+
+
     }
 };
