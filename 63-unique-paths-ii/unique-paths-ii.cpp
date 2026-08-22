3575554ced2f1b50fb8844f5ae6381a4bb5c853f
Harish Tholeti <147231266+urs-karna@users.noreply.github.com>
2026-08-22 22:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (72.46%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -20,6 +20,28 @@ public:
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return find(n-1,m-1,obstacleGrid,dp);
+        // return find(n-1,m-1,obstacleGrid,dp);
+
+        for(int i =0 ;i < n;i++) {
+            for(int j =0;j<m;j++) {
+
+                if(i == 0 && j ==0 && obstacleGrid[i][j] ==  0)  {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                if(obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0; 
+                    continue;
+                }
+                int up =0, left =0;
+
+                if( i > 0) up = dp[i-1][j];
+                if(j> 0) left = dp[i][j-1];
+
+                dp[i][j]  = up+ left;
+            }
+        }
+
+        return dp[n-1][m-1];
     }
 };
