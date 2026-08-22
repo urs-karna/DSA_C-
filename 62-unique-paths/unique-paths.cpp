a7c8227cb2efe27d5cd3828a871aaa9b4ac41808
Harish Tholeti <147231266+urs-karna@users.noreply.github.com>
2026-08-22 19:25:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (31.13%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+
+private: 
+   int find(int m , int n, vector<vector<int>>& dp ) {
+
+    if( m == 0 && n == 0) return 1;
+    if( m < 0 ||  n < 0 ) return 0;
+    if( dp[m][n] != -1) return dp[m][n];
+    int left = find(m - 1, n,dp);
+
+    int right = find(m, n-1,dp);
+
+    return dp[m][n] = left + right;
+   }
+public:
+    int uniquePaths(int m, int n) {
+
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+
+        return find(m-1, n-1,dp);
+        
+    }
+};
