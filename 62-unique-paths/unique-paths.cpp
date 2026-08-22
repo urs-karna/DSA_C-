8283248aec659a9704f8312fa762ca816341435c
Harish Tholeti <147231266+urs-karna@users.noreply.github.com>
2026-08-22 21:25:30 +0530

    Time: 2 ms (20.91%) | Memory: 9.3 MB (54.78%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -17,7 +17,28 @@ public:
 
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
 
-        return find(m-1, n-1,dp);
+        dp[0][0]= 1;
+
+        for(int i =0;i < m ;i++) {
+ 
+            for(int j =0; j< n ;j++) {
+               
+               if( i ==0 && j ==0) continue;
+                dp[i][j] = 0;
+                
+                int left =0 , right =0;
+                if(i > 0) {
+                 left = dp[i-1][j]; }
+
+                if ( j > 0) {
+                 right = dp[i][j-1];
+                }
+
+                dp[i][j] = left + right ;
+            }
+        }
+
+        return dp[m-1][n-1];
         
     }
 };
